54ce58958f3fb4f1566707e9bf97445b99fd7f60
Monis Mehdi <mehdi.monis@gmail.com>
2019-05-27 23:40:48 +0530

    fixed filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,31 +19,81 @@ export class AppComponent {
   dataColumn : any;
   dataSource : any;
 
+  filteredData : any;
+
   colName : String;
+  ccyPairFilter: String;
+  statusFilter: String;
 
   constructor(private blatterService : BlatterService) {}
 
   ngOnInit(){
     this.getColumn();
-    this.getData();    
+    this.getData(); 
   }
 
   getColumn(){
     this.blatterService.getColumn().subscribe((data: Array<object>) => {
       this.dataColumn = data;
-      console.log(data);
     })
+  }
+
+
+
+  filterGrid(){
+    if((this.ccyPairFilter == undefined || this.ccyPairFilter == '') &&  (this.statusFilter == undefined || this.statusFilter == '')){
+      this.resetGrid();
+      return;
+    }
+
+    this.filteredData = this.dataSource;
+    if(!this.isEmpty(this.ccyPairFilter)){
+      this.filteredData = this.filterForCcyPair(this.dataSource);
+    }    
+    
+    if(!this.isEmpty(this.statusFilter)){
+      this.filteredData = this.filterForStatus(this.filteredData);
+    }
+
+  }
+
+  resetGrid(){
+    this.filteredData = this.dataSource;
+  }
+
+
+  filterForCcyPair(orders){
+    let filteredOrders = [];
+    for(let order of orders){
+      if(order['CcyPair'].includes(this.ccyPairFilter)){
+        filteredOrders.push(order);
+      }
+    }
+    return filteredOrders;
+  }
+
+  filterForStatus(orders){
+    let filteredOrders = [];
+    for(let order of orders){
+      if(order['Status'].includes(this.statusFilter) ){
+        filteredOrders.push(order);
+      }
+    }
+    return filteredOrders;
   }
   
   
   getData(){
     this.blatterService.getData().subscribe((data: Array<object>) => {
       this.dataSource = data;
-      console.log(data);
-      
+      this.filteredData = data;
     }, 
       error => console.log('Error fetching values')
     );
+  }
+
+  isEmpty(value: any){
+    return value == '' || value == undefined;
   }
 
 }
